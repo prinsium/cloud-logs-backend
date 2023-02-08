36d4dd476e478e3716be2db48081857309513fde
3vdi <unitcomics.world@proton.me>
2023-02-08 18:40:18 +0530

    changed some code of db.js and index.js

--- db.js
+++ db.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://127.0.0.1:27017";
+
+// const connectToMongo = ()=>{
+//     mongoose.connect(mongoURI, ()=>{
+//         console.log("connected to mongoose successfully")
+//     })
+// }
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
+    mongoose.connect(mongoURI).then(()=>{
         console.log("connected to mongoose successfully")
-    })
-}
+    }).catch((err)=> console.log(err));
+};
+
+
 module.exports = connectToMongo;
