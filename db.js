04b1b1ff0668b91ca759bc57c17478ed463f79cf
3vdi <unitcomics.world@proton.me>
2023-02-09 10:25:06 +0530

    added code for authenticating a user while logging in

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const mongoURI = "mongodb://127.0.0.1:27017";
 //     })
 // }
 
+
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI).then(()=>{
         console.log("connected to mongoose successfully")
